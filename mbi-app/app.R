c44ff55e6a6e93ea6e1a9115a93eb452e1638f0f
Artur Brodzki <artur.brodzki@gmail.com>
2018-01-20 15:57:11 +0100

    PCA & dendrogram done

--- mbi-app/app.R
+++ mbi-app/app.R
@@ -5,30 +5,62 @@ library(SNPRelate)
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
-  # App title ----
-  titlePanel("MBI-17Z"),
-  
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
     
     # Sidebar panel for inputs ----
     sidebarPanel(
       
-      # Input: Slider for the number of bins ----
+      h1("Settings"),
+      
+      selectInput(inputId = "format", 
+                  label = "File format", 
+                  choices = c("GDS", "VCF"), 
+                  selected = "GDS"),
+      
+      textInput(inputId = "vcf", 
+                label = "Path to genome file", 
+                value = "~/Projekty/MBI/mbi-app/chr22.gds"),
+      
+      textInput(inputId = "pop", 
+                label = "Path to file with population labels", 
+                value = "~/Projekty/MBI/mbi-app/chr22.pop"),
+      
+      textInput(inputId = "ranges", 
+                label = "Path to file with chromosome ranges to analyze", 
+                value = ""),
+      
+      checkboxInput(inputId = "ld", 
+                    label = "Use LD pruning (may take a longer time to complete)", 
+                    value = FALSE),
+      
       sliderInput(inputId = "maf",
                   label = "MAF threshold:",
                   min = 0,
-                  max = 0.45,
-                  value = 0.05)
+                  max = 0.5,
+                  value = 0.3),
+      
+      selectInput(inputId = "ev1", 
+                  label = "First eigenvector (horizontal axis)", 
+                  choices = c(1:20), 
+                  selected = 1),
+      
+      selectInput(inputId = "ev2", 
+                  label = "Second eigenvector (vertical axis)", 
+                  choices = c(1:20), 
+                  selected = 2),
+      
+      checkboxInput(inputId = "clust", 
+                    label = "Generate dendrogram (hierarchical clustering) on the data", 
+                    value = FALSE)
       
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
       
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-      
+      plotOutput(outputId = "pcaPlot"),
+      plotOutput(outputId = "dendPlot")
     )
   )
 )
@@ -36,36 +68,133 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
+  # Make PCA plot
+  output$pcaPlot <- renderPlot({
     
-    chr22 <- snpgdsOpen("~/Projekty/MBI/mbi-app/chr22.gds")
-    snpsetcsv <- read.csv("~/Projekty/MBI/chr22-pruned.csv")
-    snpsetcsv.id = unlist(snpsetcsv$chr22)
-    set_maf <- input$maf
-    pca <- snpgdsPCA(chr22, snp.id = snpsetcsv.id, num.thread=4, maf = set_maf)
+    # Load VCF file
+    if(input$format == "VCF") {
+      snpgdsVCF2GDS(input$vcf, paste0(input$vcf,".gds"), method="biallelic.only")
+      chr22 <- snpgdsOpen(paste0(input$vcf,".gds"))
+    }
+    # Load GDS file
+    if(input$format == "GDS") {
+      chr22 <- snpgdsOpen(input$vcf)
+    }
+    
+    # Do LD-pruning if required
+    if(input$ld) {
+      snpset <- snpgdsLDpruning(
+        chr22, 
+        ld.threshold=0.2, 
+        num.thread = 4,
+        maf = input$maf)
+      snpset.id <- unlist(snpset)
+    }
+    else {
+      snpset.id = NULL
+    }
+    
+    # Choose chromosome ranges of required
+    if(input$ranges != "") {
+      rng <- read.table(input$ranges, sep = '\t', header = TRUE)
+      num_rng = length(rng$chromosomes)
+      selected = c()
+    }
+    
+    # Calculate PCA on samples
+    pca <- snpgdsPCA(
+      chr22, 
+      snp.id = snpset.id, 
+      num.thread=4, 
+      maf = input$maf)
+    # Close unnecessary GDS file
     snpgdsClose(chr22)
-    pop_code <- read.table("~/Projekty/MBI/integrated_call_samples_v3.20130502.ALL.panel", sep = "", header = T, nrows = 2504)
+    # Read population codes
     # AFR = 1
     # AMR = 2
     # EAS = 3
     # EUR = 4
     # SAS = 5
+    pop_code <- read.table(input$pop, 
+                           sep = "", 
+                           header = T, 
+                           nrows = 2504)
+    # Make a plot
+    eigenvector1 <- strtoi(input$ev1)
+    eigenvector2 <- strtoi(input$ev2)
     tab <- data.frame(sample.id = pca$sample.id,
                       pop = pop_code$super_pop,
-                      EV1 = pca$eigenvect[,1],    # the first eigenvector
-                      EV2 = pca$eigenvect[,4],    # the second eigenvector
+                      EV1 = pca$eigenvect[,eigenvector1],    # the first eigenvector
+                      EV2 = pca$eigenvect[,eigenvector2],    # the second eigenvector
                       stringsAsFactors = FALSE)
-    plot(tab$EV2, tab$EV1, xlab="eigenvector 2", ylab="eigenvector 1", col=as.integer(tab$pop))
+    plot(tab$EV2, 
+         tab$EV1, 
+         xlab="Eigenvector 1", 
+         ylab="Eigenvector 2", 
+         col=as.integer(tab$pop), 
+         main = paste0("Populations PCA, MAF >= ", input$maf))
     
+    legend("bottomright", 
+           legend=levels(pop_code$super_pop), 
+           text.col=1:nlevels(pop_code$super_pop))
     
+  })
+  
+  output$dendPlot = renderPlot({
+    
+    if(input$clust) {
+      # Load VCF file
+      if(input$format == "VCF") {
+        snpgdsVCF2GDS(input$vcf, paste0(input$vcf,".gds"), method="biallelic.only")
+        chr22 <- snpgdsOpen(paste0(input$vcf,".gds"))
+      }
+      # Load GDS file
+      if(input$format == "GDS") {
+        chr22 <- snpgdsOpen(input$vcf)
+      }
+      
+      # Do LD-pruning if required
+      if(input$ld) {
+        snpset <- snpgdsLDpruning(
+          chr22, 
+          ld.threshold=0.2, 
+          num.thread = 4,
+          maf = input$maf)
+        snpset.id <- unlist(snpset)
+      }
+      else {
+        snpset.id = NULL
+      }
+      
+      # Read population codes
+      # AFR = 1
+      # AMR = 2
+      # EAS = 3
+      # EUR = 4
+      # SAS = 5
+      pop_code <- read.table(input$pop, 
+                             sep = "", 
+                             header = T, 
+                             nrows = 2504)
+      pop <- pop_code$super_pop
+      # Do IBS anaysis
+      ibs <- snpgdsIBS(chr22, 
+                       snp.id = snpset.id, 
+                       maf = input$maf,
+                       num.thread=4)
+      # Close unnecessary GDS file
+      snpgdsClose(chr22)
+      # Do hierarchical clustering
+      ibs.hc <- snpgdsHCluster(ibs)
+      rv <- snpgdsCutTree(ibs.hc, samp.group=as.factor(pop))
+      
+      plot(rv$dendrogram, 
+           leaflab="none", 
+           main=paste0("Populations dendrogram, MAF >= ", 
+                       input$maf, ", ", 
+                       length(levels(as.factor(pop))), " groups"))
+      legend("bottomright", legend=levels(pop), text.col=1:nlevels(pop))
+    }
   })
   
 }
